fd51303a80cc656fcc6b8925bff92de918e2b4a0
uikeysheetal <112274728+uikeysheetal@users.noreply.github.com>
2023-02-28 02:29:46 +0530

    'my-app'

---
+++ src/components/basics/menuApi.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-unused-vars */
+
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "Maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "Aalu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/pasta.jpg",
+      name: "Pasta",
+      category: "breakfast",
+      price: "100₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/chhole bhature.jpg",
+      name: "chhola Bhatura",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/biryani.jpg",
+      name: "Biryani",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/Tandoori-chicken-tikka.jpg",
+      name: "Tandoori-chicken-tikka",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/ras malai.jpg",
+      name: "Ras Malai",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/Malai-Kofta.jpg",
+      name: "Malai-Kofta",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "Samasa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
